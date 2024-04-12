0a638df349c5cbf2e49db22063f38da12d94683b
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-04-12 09:18:07 -0600

    userrecommendation

--- brickwell2/Models/ViewModels/PaginationListViewModel.cs
+++ brickwell2/Models/ViewModels/PaginationListViewModel.cs
@@ -1,11 +1,12 @@
 ﻿namespace brickwell2.Models.ViewModels
 {
-    public class PaginationListViewModel
-    {
-        public IQueryable<Product>? Products { get; set; }
-        public IQueryable<AspNetUser>? AspNetUsers { get; set; }
-        public IQueryable<Order>? Orders { get; set; }
-        public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo(); 
-        public string? CurrentCategory { get; set; }
-    }
+	public class PaginationListViewModel
+	{
+		public IQueryable<Product>? Products { get; set; }
+		public IQueryable<AspNetUser>? AspNetUsers { get; set; }
+		public IQueryable<Order>? Orders { get; set; }
+		public PaginationInfo PaginationInfo { get; set; } = new PaginationInfo();
+		public string? CurrentCategory { get; set; }
+		public string? CurrentColor { get; set; }
+	}
 }
