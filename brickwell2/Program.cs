c1c94d9bade096666c146006328fdae0629e3c31
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2024-04-12 07:28:31 -0600

    Revert "Update Program.cs"
    This reverts commit 01e082b8f8d49d3dd7e87d69e4d9312f96fa03e5.

--- brickwell2/Program.cs
+++ brickwell2/Program.cs
@@ -1,27 +1,41 @@
-using brickwell2.Data;
-using brickwell2.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.ML.OnnxRuntime;
+using System;
+using System.Threading.Tasks;
+using brickwell2.Data;
+using brickwell2.Models;
 
-public class Program
+namespace brickwell2
 {
-    public static async Task Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-        builder.Services.Configure<CookiePolicyOptions>(options =>
+        public static async Task Main(string[] args)
         {
-            // This lambda determines whether user consent for non-essential 
-            // cookies is needed for a given request.
-            options.CheckConsentNeeded = context => true;
-            options.MinimumSameSitePolicy = SameSiteMode.None;
-            options.ConsentCookieValue = "true";
-        });
-        var services = builder.Services;
-        var configuration = builder.Configuration;
+            // Load the ONNX model
+            var session = LoadModel();
 
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Configure the services
+            ConfigureServices(builder.Services, builder.Configuration, session);
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline
+            Configure(app);
+
+            await app.RunAsync();
+        }
+
+        private static InferenceSession LoadModel()
         {
-<<<<<<< HEAD
             try
             {
                 return new InferenceSession("intex_model.onnx");
@@ -33,27 +47,15 @@ public class Program
                 return null;
             }
         }
-=======
-            options.UseSqlServer(builder.Configuration["ConnectionStrings:LegoConnection"]);
-        });
->>>>>>> parent of 4359518 (getting the fraud pred to work)
 
-        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-            .AddRoles<IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-
-        builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
-        builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
-
-        builder.Services.AddDbContext<LegoDbContext>(options =>
+        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration, InferenceSession session)
         {
-            //options.UseSqlite(builder.Configuration["ConnectionStrings:LegoConnection"]);
-            options.UseSqlServer(builder.Configuration["ConnectionStrings:LegoConnection"]);
-        });
+            services.AddSingleton(session); // Register the session as a singleton
+            services.AddControllersWithViews();
+            // Register ZooContext with the DI container
+            // services.AddDbContext<LegoDbContext>(options =>
+            //     options.UseSqlite(configuration.GetConnectionString("IntexDatabase")));
 
-<<<<<<< HEAD
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential 
@@ -127,133 +129,63 @@ public class Program
         }
 
         private static void Configure(IApplicationBuilder app)
-=======
-        builder.Services.AddDbContext<LegoSecurityContext>(options =>
->>>>>>> parent of 4359518 (getting the fraud pred to work)
         {
-            options.UseSqlServer(builder.Configuration["ConnectionStrings:LegoConnection"]);
-            //options.UseSqlite(builder.Configuration["ConnectionStrings:LegoSecurityConnection"]);
-        });
+            // if (app.Environment.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            //     app.UseMigrationsEndPoint();
+            // }
+            // else
+            // {
+            //     app.UseExceptionHandler("/Home/Error");
+            //     app.UseHsts();
+            // }
+            var env = app.ApplicationServices.GetRequiredService<IWebHostEnvironment>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseCookiePolicy();
 
-        services.AddAuthentication().AddGoogle(googleOptions =>
-        {
-            googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-            googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-        });
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Content-Security-Policy",
+                    "default-src 'self'; script-src 'self' 'unsafe-inline'; script-src-attr 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; img-src 'self' data: https://www.lego.com https://images.brickset.com https://m.media-amazon.com https://live.staticflickr.com; frame-src 'self'; connect-src 'self' http://localhost:* wss://localhost:* ws://localhost:*; style-src-elem 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com 'unsafe-inline';");
+                await next();
+            });
 
-        builder.Services.AddScoped<ILegoRepository, EFLegoRepository>();
+            app.UseRouting();
 
-        builder.Services.AddScoped<ILegoSecurityRepository, EFLegoSecurityRepository>();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
 
-        builder.Services.AddDistributedMemoryCache();
-        builder.Services.AddSession();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("pagenumandtype", "{productCategory}/{pageNum}",
+                    new { Controller = "Home", action = "Products" });
+                endpoints.MapControllerRoute("pagination", "{pageNum}",
+                    new { Controller = "Home", action = "Products", pageNum = 1 });
+                endpoints.MapControllerRoute("productCategory", "{productCategory}",
+                    new { Controller = "Home", action = "Products", pageNum = 1 });
 
-        builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
-        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-        var app = builder.Build();
-        // builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
-        // builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
-        // var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseMigrationsEndPoint();
+                endpoints.MapRazorPages();
+            });
         }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-        app.UseCookiePolicy();
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        app.UseSession();
-        app.UseCookiePolicy();
-        
-        app.Use(async (context, next) =>
-        {
-            context.Response.Headers.Add("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; script-src-attr 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; img-src 'self' data: https://www.lego.com https://images.brickset.com https://m.media-amazon.com https://live.staticflickr.com; frame-src 'self'; connect-src 'self' http://localhost:* wss://localhost:* ws://localhost:*; style-src-elem 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com 'unsafe-inline';");
-            await next();
-        });
-
-        app.UseRouting();
-
-        app.UseAuthentication();
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.MapControllerRoute("pagenumandtype", "{productCategory}/{pageNum}",
-            new { Controller = "Home", action = "Products" });
-        app.MapControllerRoute("pagination", "{pageNum}",
-            new { Controller = "Home", action = "Products", pageNum = 1 });
-        app.MapControllerRoute("productCategory", "{productCategory}",
-            new { Controller = "Home", action = "Products", pageNum = 1 });
-
-
-        app.MapRazorPages();
-
-        //using (var scope = app.Services.CreateScope())
-        //{
-        //    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-        //    var roles = new[] { "Admin", "Customer" };
-
-        //    foreach (var role in roles)
-        //    {
-        //        if (!await roleManager.RoleExistsAsync(role))
-        //            await roleManager.CreateAsync(new IdentityRole(role));
-        //    }
-        //}
-
-        //using (var scope = app.Services.CreateScope())
-        //{
-        //    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-
-        //    string email = "admin@section210.com";
-        //    string password = "Test12345!";
-
-        //    // Check if the user already exists
-        //    var user = await userManager.FindByEmailAsync(email);
-        //    if (user == null)
-        //    {
-        //        // Create a new user
-        //        user = new IdentityUser { UserName = email, Email = email };
-        //        var createUserResult = await userManager.CreateAsync(user, password);
-
-        //        // Check if user creation was successful
-        //        if (createUserResult.Succeeded)
-        //        {
-        //            // User created successfully, now add to the "Admin" role
-        //            var addToRoleResult = await userManager.AddToRoleAsync(user, "Admin");
-
-        //            // Check if adding to role was successful
-        //            if (!addToRoleResult.Succeeded)
-        //            {
-        //                // User role assignment failed, log the errors
-        //                var roleErrors = addToRoleResult.Errors.Select(e => e.Description).ToList();
-        //                string roleErrorMessages = string.Join(", ", roleErrors);
-        //                throw new InvalidOperationException($"User created but adding user with email '{email}' to role 'Admin' failed. Details: {roleErrorMessages}");
-        //            }
-        //        }
-        //        else
-        //        {
-        //            // User creation failed, log the errors
-        //            var userErrors = createUserResult.Errors.Select(e => e.Description).ToList();
-        //            string userErrorMessages = string.Join(", ", userErrors);
-        //            throw new InvalidOperationException($"Unexpected error occurred creating user with email '{email}'. Details: {userErrorMessages}");
-        //        }
-        //    }
-        //}
-
-
-        app.Run();
     }
 }
